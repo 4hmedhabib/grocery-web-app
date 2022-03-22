5eec1ee912e17e8a1ddc918b854cb7f4c63e8295
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-22 18:04:47 +0300

    added: showcase

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,16 +6,7 @@ import Showcase from "./Showcase/Showcase";
 const Main = () => {
   return (
     <main>
-      <section>
-        <div id="info_container">
-          <h1>Freshly picked from nature</h1>
-          <p>
-            We provides you with various fresh grocery with 10% off for every
-            $50 purchase
-          </p>
-        </div>
-        <div></div>
-      </section>
+      <Showcase />
     </main>
   );
 };
--- src/components/Main/Showcase/Showcase.jsx
+++ src/components/Main/Showcase/Showcase.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import classes from "./Showcase.module.css";
 
 const Showcase = () => {
-  return <div>Showcase</div>;
+  return (
+    <section className={classes.showcase_container}>
+      <div id="info_container">
+        <h1 className={classes.title}>
+          Freshly <span className="text-orange-500">picked</span> <br /> from
+          <span className="text-primary"> nature</span>
+        </h1>
+        <p className={classes.desc}>
+          We provides you with various fresh grocery with 10% <br /> off for
+          every $50 purchase
+        </p>
+      </div>
+      <div className={classes.buttons_container}>
+        <button className={classes.btn_main}>View Sale</button>
+        <button className={classes.btn_normal}>Shop All</button>
+      </div>
+    </section>
+  );
 };
 
 export default Showcase;
