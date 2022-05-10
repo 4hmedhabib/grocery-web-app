e4fe581f9c0db047b06c6e32eb857099f2be2d69
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-05-10 14:34:42 +0300

    added: Products Component

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,25 @@
+const Product = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        {/* discount */}
+        <span>%25 discount</span>
+      </div>
+      {/* Title */}
+      <h1>Fresh Strawberry</h1>
+
+      {/* Price */}
+      <div>
+        <span>$8.99</span>
+        <span>$12.99</span>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
