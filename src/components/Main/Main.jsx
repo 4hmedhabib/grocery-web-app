81b281e34847c2b0d21a3906f326a10dfc050d45
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-05-10 14:34:52 +0300

    added: Intro

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // components
 import Showcase from "./Showcase/Showcase";
 import WhatWeDo from "./WhatWeDo/WhatWeDo";
+import Intro from "../Products/Intro";
 
 const Main = () => {
   return (
@@ -10,6 +11,19 @@ const Main = () => {
       <Showcase />
 
       <WhatWeDo />
+
+      <Intro />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </main>
   );
 };
