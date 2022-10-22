5ca330002175cf08cb8cf621010f4bb796549931
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-10-22 19:39:04 +0300

    added: subscription

--- src/components/Main/Subscription/Subscribe.jsx
+++ src/components/Main/Subscription/Subscribe.jsx
@@ -1,9 +1,9 @@
 const Subscribe = () => {
   return (
     <div className="bg-blue-50/50 flex flex-col justify-center items-center sm:flex-row sm:justify-evenly py-8">
-      <div className="w-full sm:max-w-2xl flex-col sm:flex-row items-center justify-center sm:justify-between bg-red-100">
+      <div className="w-full sm:max-w-3xl flex flex-col items-center  md:flex-row  md:justify-between gap-3">
         {/* Information */}
-        <div className="flex flex-col justify-center items-start">
+        <div className="flex flex-col justify-center items-center sm:items-start">
           {/* Title */}
           <h1 className="font-viga text-lg mb-1">Save 20% on first shopping</h1>
           {/* Desc */}
@@ -19,8 +19,8 @@ const Subscribe = () => {
             name="email"
             className="px-2 rounded-tl-full rounded-bl-full outline-none py-2 font-nunito font-semibold tracking-wider w-full"
           />
-          <button className="bg-primary absolute right-0 bottom-0 top-0">
-            <img src="" alt="" />
+          <button className="bg-primary absolute right-0 bottom-0 top-0 w-32 flex items-center justify-center gap-2 text-white font-nunito tracking-wider">
+            <img src="/assets/images/send.png" alt="" className="h-5" />
             Subscribe
           </button>
         </div>
